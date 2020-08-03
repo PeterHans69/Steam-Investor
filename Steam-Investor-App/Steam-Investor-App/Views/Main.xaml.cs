74f883ad367efa30a69f0812f1efcfce576ac5d0
Shynex1234 <62182341+Shynex1234@users.noreply.github.com>
2020-08-03 20:15:42 +0200

    added chart package

--- Steam-Investor-App/Steam-Investor-App/Views/Main.xaml.cs
+++ Steam-Investor-App/Steam-Investor-App/Views/Main.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace Steam_Investor_App.Views
 {
@@ -23,6 +25,12 @@ namespace Steam_Investor_App.Views
         public UserControl1()
         {
             InitializeComponent();
+            Values1 = new ChartValues<double> { 3, 4, 6, 3, 2, 6 };
+            Values2 = new ChartValues<double> { 5, 3, 5, 7, 3, 9 };
+
+            DataContext = this;
         }
+        public ChartValues<double> Values1 { get; set; }
+        public ChartValues<double> Values2 { get; set; }
     }
 }
