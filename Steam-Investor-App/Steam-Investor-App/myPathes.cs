ec53364d1f993c5d467eedf904c3e620abea7f0a
Shynex1234 <62182341+Shynex1234@users.noreply.github.com>
2020-09-03 21:46:34 +0200

    Updates for Debugging

--- Steam-Investor-App/Steam-Investor-App/myPathes.cs
+++ Steam-Investor-App/Steam-Investor-App/myPathes.cs
@@ -7,16 +7,16 @@ namespace Steam_Investor_App
 {
     public static class MyPathes
     {
-        //public static string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)+"..\\..\\..";// For Debugging in the 
+        public static string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)+"..\\..\\..";// For Debugging in the 
         
 
-        //public static string MySteamItems = path+"\\SteamData\\MySteamItems.json";
-        //public static string SteamItems = path+"\\SteamData\\SteamItems.json";
+        public static string MySteamItems = path+"\\SteamData\\MySteamItems.json";// For Debugging in the 
+        public static string SteamItems = path+"\\SteamData\\SteamItems.json";// For Debugging in the 
 
-        public static string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); //For the installed app
+        //public static string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); //For the installed app
 
-        public static string MySteamItems = path + "\\MySteamItems.json";
-        public static string SteamItems = path + "\\SteamItems.json";
+        //public static string MySteamItems = path + "\\MySteamItems.json";//For the installed app
+        //public static string SteamItems = path + "\\SteamItems.json";//For the installed app
 
 
         public static string charData = path+"\\charData.json";
